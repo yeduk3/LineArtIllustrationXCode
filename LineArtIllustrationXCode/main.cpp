31935642e240596fe1e908f5775dd578b705dac6
yeduk3 <yeduk33@gmail.com>
2025-03-07 17:29:46 +0900

    Initial Commit

---
+++ LineArtIllustrationXCode/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LineArtIllustrationXCode
+//
+//  Created by 이용규 on 3/7/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
